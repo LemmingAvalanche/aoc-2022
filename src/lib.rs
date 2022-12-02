41d659fcff42bd01eda3c794e9ef391a68359698
Kristoffer Haugsbakk <code@khaugsbakk.name>
2022-12-02 14:34:37 +0100

    Start AOC 2022

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+// No Rust code until day 5
+pub mod day5;
+
+aoc_lib!{
+    year = 2020
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::fs;
+
+fn main() {
+    // I just update this function for each problem that I bother to solve,
+    // setting the input file path and the function call.
+
+    // Example from 2020:
+  // let input = fs::read_to_string(
+  //      "/home/kristoffer/programming/advent-of-code-2020/day18-input.txt")
+  //      .unwrap();
+}
