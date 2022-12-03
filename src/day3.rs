e00444b27ff9f613a1d0fc344bfcd1ca4ab29d46
Kristoffer Haugsbakk <code@khaugsbakk.name>
2022-12-03 09:08:11 +0100

    3: part 2

--- src/day3.rs
+++ src/day3.rs
@@ -2,16 +2,37 @@ use byte_set::ByteSet;
 
 // as usual
 pub fn part1_solve(input: &str) -> u32 {
-    input.lines().map(|l| score_rucksack(l)).sum()
+    input.lines().map(|l| score_rucksack_part1(l)).sum()
 }
 
-fn score_rucksack(rucksack: &str) -> u32 {
+pub fn part2_solve(input: &str) -> u32 {
+    input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|lines| score_rucksacks_part2(lines[0], lines[1], lines[2]))
+        .sum()
+}
+
+fn score_rucksack_part1(rucksack: &str) -> u32 {
     let (comp1, comp2) = rucksack.split_at(rucksack.len() / 2);
     let comp1_set = str_to_set(comp1);
     let comp2_set = str_to_set(comp2);
     let intersection = comp1_set.intersection(comp2_set);
+    score(intersection)
+}
+
+fn score_rucksacks_part2(r1: &str, r2: &str, r3: &str) -> u32 {
+    let rset1 = str_to_set(r1);
+    let rset2 = str_to_set(r2);
+    let rset3 = str_to_set(r3);
+    let intersection = rset1.intersection(rset2).intersection(rset3);
+    score(intersection)
+}
+
+fn score(set: ByteSet) -> u32 {
     // only one
-    let common = intersection.into_iter().nth(0).expect("bad input?");
+    let common = set.into_iter().nth(0).expect("bad input?");
     // upper-case
     if common <= 90 {
         common as u32 - 38
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use std::fs;
-
 use advent_of_code_2022::day3::*;
+
+static input: &str = include_str!("/home/kristoffer/programming/advent-of-code-2022/day3-input.txt");
 
 fn main() {
     // I just update this function for each problem that I bother to solve,
     // setting the input file path and the function call.
-
-    let input =
-        fs::read_to_string("/home/kristoffer/programming/advent-of-code-2022/day3-input.txt")
-            .unwrap();
-    println!("{}", part1_solve(&input));
+    println!("{}", part2_solve(&input));
 }
